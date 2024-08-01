2c0187fa94236eba1abfa3f028d47776be0ef90e
Thiago William <117531207+thiagowmoreira@users.noreply.github.com>
2024-08-01 11:13:33 -0300

    update lado while
    atualização do laço while e break

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import random
 
+Epar = True
 
-num = input("digite um numero inteiro: ")
-if num.isnumeric():
-    if int(num) % 2 == 0:
-        print(f"{num} é par")
+while Epar:
+    num = input("digite um numero inteiro: ")
+    if num == "break":
+        Epar = False
+        break
+
+    if num.isnumeric():
+        if int(num) % 2 == 0:
+            print(f"{num} é par")
+        else:
+            print(f"{num} é impar")
     else:
-        print(f"{num} é impar")
-else:
-    print("digite somente numeros")
+        print("digite somente numeros")
 
